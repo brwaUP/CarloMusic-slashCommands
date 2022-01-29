1ca9df1f6901252329d332853f3510c6fd174a02
brwaUP <83411029+brwaUP@users.noreply.github.com>
2022-01-30 02:29:53 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    token: "",
-    prefix: "",
-    ownerID: "",
+    token: "OTI2MjIzOTk4NDc4NTg5OTYy.Yc4jUQ.RsaBr1AloDDkmQEtMeJfO976qY0",
+    prefix: "+",
+    ownerID: "787384416892223520",
     SpotifyID: "",
     SpotifySecret: "",
-    mongourl: "",
-    embedColor: "",
+    mongourl: "AIzaSyDExCZ2UhM3i5WGs5hNYQmIiwswkxbD",
+    embedColor: "RANDOM",
     logs: "",
 
   nodes: {
